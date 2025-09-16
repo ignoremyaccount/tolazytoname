adfc42d3139ee325a98cfd72be307b1ba65921e5
ignoremyaccount <jw90457@leisd.net>
2025-09-16 15:15:20 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,28 @@
-enum numlit {
-	binlit, octlit, declit, hexlit
-};
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+long long int parse_binary(uint8_t *n, size_t s) {
+	long long int endval = 0;
+	for (int i = s; i > 0; i--) {
+		if (*n != '1' || *n != '0')
+			return -1
+
+		if (*n == '1') {
+			
+		}
+		n++;
+	} 
+}
+
+long long int parse_numlit(uint8_t *n, size_t s) {
+	if (s > 2) {
+		if (memcmp(n, "0b", 2) == 0) {
+			
+		}
+	}
+}
+
+/*
+
+*/
